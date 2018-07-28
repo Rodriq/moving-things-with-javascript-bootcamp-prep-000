1b4dc6423d45787ff6d6defebc3c25e52c02f4e9
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-28 17:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(leftNumbers, 10)
+  var right = parseInt(rightNumbers, 10)
  
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`
   }
 }
 document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
+  if (e.which === 38) {
     moveDodgerLeft()
   }
 })
